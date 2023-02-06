3a76732639bd8acbf5644865631ea75e1feadd79
beyazanuroter <beyazanuroter3@gmail.com>
2023-02-06 18:07:46 +0300

    yaş problemi yaptık

---
+++ yas_problem.py
@@ -0,0 +1,9 @@
+birthdate = int(input("Enter your birthdate: "))
+
+age = 2023 - birthdate
+print(age)
+
+birthdate = int(input("Enter your birthdate: "))
+
+age = 2023 - birthdate
+print("You are", age, "years old.")
