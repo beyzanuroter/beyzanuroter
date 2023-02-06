e8cd2aa36cdb7e513b5af88f20f8bcb81776be24
beyazanuroter <beyazanuroter3@gmail.com>
2023-02-06 23:03:37 +0300

    dikdörtgenin alanı ve çevresi

---
+++ dikdörtgen.py
@@ -0,0 +1,7 @@
+print("Welcome scriptime!")
+kısaKenar = input("Kısa kenarı giriniz")
+uzunKenar = input("Uzun kenarı giriniz")
+cevre = (int(uzunKenar)+int(kısaKenar))*2
+alan = int(uzunKenar)*int(kısaKenar)
+print("Çevre:",cevre)
+print("Alan:",alan)
--- yas_problem.py
+++ yas_problem.py
@@ -7,3 +7,9 @@ birthdate = int(input("Enter your birthdate: "))
 
 age = 2023 - birthdate
 print("You are", age, "years old.")
+
+
+
+
+
+
