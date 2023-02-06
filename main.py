92713238e4d16515afc6f4058db662887d4d7ac7
beyazanuroter <beyazanuroter3@gmail.com>
2023-02-06 14:22:41 +0300

    veri tipleri

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+print("New project")
+ilk_numara =input("Enter first number: ")
+ikinci_numara =input("Enter second number: ")
+ilk_num_int =int(ilk_numara)
+ikinci_num_int =int(ikinci_numara)
+
+toplam = ilk_num_int + ikinci_num_int
+
+
+toplam_integer = int(toplam)
+
+
+print(toplam)
+
+
+
+
 print("Hello world!")
+
+
+
+beyza_list = [1,2,3,4,5]
+print(beyza_list)
+
+asude_list =[1,"abc",(2,3),{2}]
+print(asude_list)
+
+beryesaDict = {
+    "berk" : [1,2,3]
+             }
+print(beryesaDict)
+
+aslidict = {
+    "asli" :  {"serin": 12}
+           }
+print(aslidict)
+
+dicle_tuple =(3,"abc",None,True)
+print(dicle_tuple)
+
